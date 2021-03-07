8abf83026bc0c935f4eae2d7693e5356523eea7d
nguyen <tranhoangnguyen.1312@gmail.com>
2021-03-08 00:23:06 +0700

    Update code

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -47,10 +47,14 @@ const Projects = () => {
                         <h3 className="project-wrapper__text-title">{title || 'Project Title'}</h3>
                         <div>
                           <p>
-                            {info ||
-                              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                            {'We propose the task of free-form and open-ended Visual Question Answering (VQA).'}
                           </p>
-                          <p className="mb-4">{info2 || ''}</p>
+                          <p className="mb-4">
+                            {'Given an image and a natural language question about the image, the task is to provide an accurate natural language answer. Mirroring real-world scenarios, such as helping the visually impaired, both the questions and answers are open-ended. Visual questions selectively target different areas of an image, including background details and underlying context. As a result, a system that succeeds at VQA typically needs a more detailed understanding of the image and complex reasoning than a system producing generic image captions. Moreover, VQA is amenable to automatic evaluation, since many open-ended answers contain only a few words or a closed set of answers that can be provided in a multiple-choice format. We provide a dataset containing ~0.25M images, ~0.76M questions, and ~10M answers (www.visualqa.org), and discuss the information it provides.'}
+                          </p>
+                          <p className="mb-4">
+                            {'Numerous baselines for VQA are provided and compared with human performance'}
+                          </p>
                         </div>
                         <Button>
                           <Link to="demo" smooth duration={1000}>
